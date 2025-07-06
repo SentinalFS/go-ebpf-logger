9776de449c85b1bddaeded0f413bf8f476f362e0
siddh34 <ssiddharthsutar@gmail.com>
2025-07-06 17:12:23 +0530

    feat: docker release

--- models/models.go
+++ models/models.go
@@ -1,22 +1,28 @@
 package models
 
 type BaseEventData struct {
-    Pid       uint32
-    Uid       uint32
-    Filename  [144]byte
-    Comm      [32]byte
-    Timestamp uint64
-    CgroupId  uint64
-    Otype     [16]byte
+	Pid            uint32
+	Uid            uint32
+	Filename       [176]byte
+	ParentFilename [176]byte
+	Inode          uint64
+	Comm           [32]byte
+	Timestamp      uint64
+	CgroupId       uint64
+	Otype          [16]byte
 }
 
 type RenameData struct {
-    Pid         uint32
-    Uid         uint32
-    OldFileName [144]byte
-    NewFileName [144]byte
-    Comm        [32]byte
-    Timestamp   uint64
-    CgroupId    uint64
-    Otype       [16]byte
+	Pid               uint32
+	Uid               uint32
+	OldFileName       [176]byte
+	OldParentFilename [176]byte
+	NewFileName       [176]byte
+	NewParentFilename [176]byte
+	InodeOld          uint64
+	InodeNew          uint64
+	Comm              [32]byte
+	Timestamp         uint64
+	CgroupId          uint64
+	Otype             [16]byte
 }
